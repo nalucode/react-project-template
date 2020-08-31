ba5e1d0a0ea73114f7f9b529335bea6a8b680202
Luan Vinicius <nalucode@hotmail.com>
2020-08-30 22:07:53 -0300

    Launch

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>React Project Template</h1>
+      <span>
+        <a href="https://github.com/nalucode">Luan Vinicius</a>
+      </span>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import 'regenerator-runtime/runtime';
+
+import App from './App';
+
+render(<App />, document.getElementById('app'));
